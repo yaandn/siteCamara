af5f261e8411a1b07e858bb68ed5e965192b595d
Yan Di Nápoli Araújo <dinapoliyan@gmail.com>
2024-02-20 15:32:47 -0300

    token como string

--- src/components/InstaFeed/index.jsx
+++ src/components/InstaFeed/index.jsx
@@ -1,16 +1,25 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-import { Container, Notice, Image, Text, Card, Title, Notices, Button } from './styles';
+import {
+  Container,
+  Notice,
+  Image,
+  Text,
+  Card,
+  Title,
+  Notices,
+  Button,
+} from "./styles";
 
 import { FaInstagram } from "react-icons/fa";
-
 
 export default function InstaFeed() {
   const [feedList, setFeedList] = useState([]);
 
   async function getInstaFeed() {
-    const token = import.meta.env.VITE_INSTA_TOKEN;
+    const token =
+      "IGQWRQYzBwdVFzNGJZAMXhtTjFoTDJoMTY3TUtZASktvYzhSTldmRk9GTXA3VWtiS0hvN2dyQXdrRHNXRWJrbzhQbVVTM282WGhHVzIzemxzVVRiOXpiMTlOaGlmRDBzZATVZAZAHhyZAHJpcEJBd2xsVjRGVHExbEptVkkZD";
     const fields = "media_url,media_type,permalink,caption"; // Adicionando 'caption' para a descrição
     const url = `https://graph.instagram.com/me/media?access_token=${token}&fields=${fields}`;
 
@@ -31,18 +40,17 @@ export default function InstaFeed() {
     <Container>
       <Title>Últimas noticias da Câmara municipal de Aragôiania</Title>
 
-      
-      <Notices> 
+      <Notices>
         {feedList.map((item) => {
-          if(item.media_type === "IMAGE" && item.caption ) {
-            return(
+          if (item.media_type === "IMAGE" && item.caption) {
+            return (
               <Card>
                 <Notice>
                   <Image src={item.media_url} />
-                  <div className='text-button'>
+                  <div className="text-button">
                     <Text>{item.caption}</Text>
-                    <Button>
-                    <FaInstagram size={25}/>
+                    <Button href={item.permalink}>
+                      <FaInstagram size={25} />
                       <span>Acessar a notícia no nosso instagram</span>
                     </Button>
                   </div>
--- src/components/InstaFeed/styles.js
+++ src/components/InstaFeed/styles.js
@@ -1,82 +1,76 @@
 import styled from "styled-components";
 
-
 export const Container = styled.div`
-    width: 70%;
-    margin: auto;
-    margin-top: 40px ;
-    min-height: 100vh;
-    
+  width: 70%;
+  margin: auto;
+  margin-top: 40px;
+  min-height: 100vh;
 `;
 
 export const Title = styled.span`
-   font-size: 48px;
-   margin-top :40px ;
-   margin-bottom: 50px;
-   display: block;
-
+  font-size: 48px;
+  margin-top: 40px;
+  margin-bottom: 50px;
+  display: block;
 `;
 
 export const Notices = styled.div`
-    display: flex;
-    gap: 42px;
-    width: 100%;
-    flex-wrap: wrap;
-    margin: auto;
-    margin-bottom: 40px;
+  display: flex;
+  gap: 42px;
+  width: 100%;
+  flex-wrap: wrap;
+  margin: auto;
+  margin-bottom: 40px;
 `;
 
 export const Card = styled.div`
-    width: 100%;
-    padding: 24px;
-    display: flex;
-    justify-content: center;
-    border: 1px solid var(--cor-neutra-dark);
-    border-radius: 8px;
-    background-color: var(--cor-neutra-light);
+  width: 100%;
+  padding: 24px;
+  display: flex;
+  justify-content: center;
+  border: 1px solid var(--cor-neutra-dark);
+  border-radius: 8px;
+  background-color: var(--cor-neutra-light);
 
-    .text-button {
-        display: flex;
-        flex-direction: column;
-        gap: 20px;
-    }
+  .text-button {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
 `;
 export const Notice = styled.div`
-    width: 100%;
-    display: flex;
-    gap: 32px;
-   
-
+  width: 100%;
+  display: flex;
+  gap: 32px;
 `;
 
 export const Image = styled.img`
-    width: 400px;
-    height: 90%;
+  width: 400px;
+  height: 90%;
 `;
 
 export const Text = styled.span`
-    font-size: 18px;
-    line-height: 1.5;
+  font-size: 18px;
+  line-height: 1.5;
 `;
 
 export const Button = styled.a`
-    width: 40%;
-    padding: 12px 16px;
-    cursor: pointer;
-    background-color: var(--cor-primaria);
-    color: aliceblue;
-    border-radius: 8px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 8px;
-    transition: background-color 0.3s, border-color 0.3s, color 0.3s; /* Adicionando transição */
+  width: 40%;
+  padding: 12px 16px;
+  cursor: pointer;
+  background-color: var(--cor-primaria);
+  color: aliceblue;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  transition: background-color 0.3s, border-color 0.3s, color 0.3s; /* Adicionando transição */
+  text-decoration: none;
 
-    &:hover {
-        background-color: var(--cor-primaria-light);
-        border: 2px solid var(--cor-primaria);
-        color: var(--cor-primaria);
-    }
+  &:hover {
+    background-color: var(--cor-primaria-light);
+    border: 2px solid var(--cor-primaria);
+    color: var(--cor-primaria);
+  }
 `;
-
-
