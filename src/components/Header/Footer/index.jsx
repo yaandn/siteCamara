96287f9db967827dee1a2b1177933b5265b7b472
Yan Di Nápoli Araújo <dinapoliyan@gmail.com>
2024-02-22 10:07:48 -0300

    atualização do link para transparencia

--- src/components/Header/Footer/index.jsx
+++ src/components/Header/Footer/index.jsx
@@ -1,26 +1,29 @@
 import { Container } from "./style";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   const navItens = [
-   { id:"1",title:"LEGISLAÇÃO", link: "/legislacao" },
-   { id:"2",title:"SOBRE A CÂMARA", link:"/sobre" },
-   { id:"3",title:"SESSÕES E DELIBERAÇÕES", link: "/sessoes" },
-   { id:"4",title:"NOTICIAS", link: "/noticias" },
-   { id:"5",title:"TRANSPARÊNCIA", link: "/transparencia" }
-  ]
+    { id: "1", title: "LEGISLAÇÃO", link: "/legislacao" },
+    { id: "2", title: "SOBRE A CÂMARA", link: "/sobre" },
+    { id: "3", title: "SESSÕES E DELIBERAÇÕES", link: "/sessoes" },
+    { id: "4", title: "NOTICIAS", link: "/noticias" },
+    {
+      id: "5",
+      title: "TRANSPARÊNCIA",
+      link: "https://camaraaragoiania.megasofttransparencia.com.br/",
+    },
+  ];
   return (
     <Container>
       <nav>
-       {navItens.map((item) => {
-        return (
-          <Link to={item.link} key={item.id
-          }>
-            <span>{item.title}</span>
-          </Link>
-        )
-       })}
+        {navItens.map((item) => {
+          return (
+            <Link to={item.link} key={item.id}>
+              <span>{item.title}</span>
+            </Link>
+          );
+        })}
       </nav>
     </Container>
-  )
+  );
 }
