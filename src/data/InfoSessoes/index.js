b2b3b64de3cdf502ad275912281190df5fac7389
Yan Di Nápoli Araújo <dinapoliyan@gmail.com>
2024-02-27 16:33:44 -0300

    atualização da sessão

--- src/data/InfoSessoes/index.js
+++ src/data/InfoSessoes/index.js
@@ -1,15 +1,13 @@
 export const sessionList = [
   {
-    id: "40",
-    data: "14/02/2024",
+    id: "41",
+    data: "21/02/2024",
     assuntos: [
-      "PROJETO DE LEI 1205/2023 QUE “DISPÕE SOBRE A CRIAÇÃO DO CENTRO DE ATENDIMENTO EDUCACIONAL ESPECIALIZADO – CAEE DO    MUNICÍPIO DE ARAGOIÂNIA E DÁ OUTRAS PROVIDÊNCIAS.",
-      "PROJETO DE LEI 1206/2023 QUE “ALTERA A REDAÇÃO DA LEI COMPLEMENTAR No 847/2008, DE 27 DE FEVEREIRO DE 2008 (INSTITUI O PLANO DE CARREIRA E VENCIMENTOS DOS SERVIDORES DO MAGISTÉRIO PÚBLICO DO MUNICÍPIO DE ARAGOIÂNIA), PARA A CRIAÇÃO E EXTINÇÃO DE CARGOS PÚBLICOS, NA FORMA QUE ESPECIFICA, E DÁ OUTRAS PROVIDÊNCIAS.",
-      "LOA – LEI ORÇAMENTÁRIA ANUAL",
+      "REQUERIMENTO 001/2024 DE AUTORIA DO VEREADOR JULIO CESAR BATISTA.",
     ],
     url_live: 0,
     link_pauta:
-      "https://drive.usercontent.google.com/u/0/uc?id=1DBBZUnBk-n21KMN9L84ZYlbzeBIgS5_T&export=download",
+      "https://drive.usercontent.google.com/u/0/uc?id=1BytwRgU89Ilxgw2DuCOTI6h1woOIs0wO&export=download",
   },
   {
     id: "40",
